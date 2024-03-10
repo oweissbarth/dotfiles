e68cccdfd3e5419b72ab336e113a43d67b406fa9
oweissbarth <mail@oweissbarth.de>
2024-03-10 20:10:26 +0100

    added copilot

--- nvim/.config/nvim/lua/ow/plugins.lua
+++ nvim/.config/nvim/lua/ow/plugins.lua
@@ -20,5 +20,5 @@ return{
   {"NeogitOrg/neogit", dependencies={"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}},
   {"lewis6991/gitsigns.nvim"},
 
-  {"zbirenbaum/copilot.lua"}
+  {"github/copilot.vim"}
 }
