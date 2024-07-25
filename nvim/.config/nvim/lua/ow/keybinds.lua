0c490e0cba4ea9cfc8057029881ac3ee6b3056d2
oweissbarth <mail@oweissbarth.de>
2024-07-25 20:54:16 +0200

    Nvim: added code action keybind

--- nvim/.config/nvim/lua/ow/keybinds.lua
+++ nvim/.config/nvim/lua/ow/keybinds.lua
@@ -5,3 +5,5 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>p", builtin.find_files, {})
 vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>o", builtin.lsp_document_symbols, {})
+
+vim.keymap.set("n", "<leader>q", function() vim.lsp.buf.code_action({apply = true}) end, {})
